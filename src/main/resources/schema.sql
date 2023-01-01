49b7b1901abc2ea513b8fa149279bb05491d9b70
Philipinho <16838612+Philipinho@users.noreply.github.com>
2023-01-01 22:02:12 +0000

    Add more database columns

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,13 +4,16 @@ USE `threadvoice`;
 CREATE TABLE IF NOT EXISTS `tweet_records` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
+  `user_id` varchar(100) NOT NULL,
   `mention_id`  varchar(255) DEFAULT NULL,
   `thread_id`  varchar(255) DEFAULT NULL,
   `voice_url` varchar(500) DEFAULT NULL,
   `thread_author` varchar(20) DEFAULT NULL,
+  `thread_author_id` varchar(100) DEFAULT NULL,
   `thread_excerpt` varchar(500) DEFAULT NULL,
   `thread_language` varchar(255) DEFAULT NULL,
   `time_saved` datetime DEFAULT CURRENT_TIMESTAMP,
+   `time_updated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY(`mention_id`)
 ) ENGINE=InnoDB;
